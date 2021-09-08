6adcd3158c0262cf53af14858527b8b5ed3f3965
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-09-08 00:56:51 -0400

    Added login and items actions to determine how to create and update these states

--- REDUX/actions/items.js
+++ REDUX/actions/items.js
@@ -1,1 +1,33 @@
-import { UPDATE_ITEM, REMOVE_ITEM, ADD_ITEM } from '../types'
+import { UPDATE_ITEM, DELETE_ITEM, ADD_ITEM, GET_ITEM } from '../types'
+import uuid from 'uuid'
+
+export const addItem =
+  (item, edit = false) =>
+  (dispatch) => {
+    if (!edit) {
+      const id = uuid.v4()
+      dispatch({
+        type: ADD_ITEM,
+        payload: { text: item.text, id },
+      })
+    } else {
+      dispatch({
+        type: UPDATE_ITEM,
+        payload: item,
+      })
+    }
+  }
+
+export const removeItem = (item) => (dispatch) => {
+  dispatch({
+    type: DELETE_ITEM,
+    payload: item,
+  })
+}
+
+export const getItem = (item) => (dispatch) => {
+  dispatch({
+    type: GET_ITEM,
+    payload: item,
+  })
+}
--- REDUX/actions/login.js
+++ REDUX/actions/login.js
@@ -1,1 +1,35 @@
-import { LOGIN, LOGOUT } from '../types'
+import { LOGIN, LOGIN_FAIL, LOGOUT, LOADING } from '../types'
+
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+  const body = JSON.stringify({ email, password })
+
+  try {
+    dispatch({
+      type: LOADING,
+    })
+    const res = await axios.post(
+      'http://dev.rapptrlabs.com/Tests/scripts/user-login.php',
+      body,
+      config
+    )
+    dispatch({
+      type: LOGIN,
+      payload: res.data,
+    })
+  } catch (err) {
+    dispatch({
+      type: LOGIN_FAIL,
+    })
+  }
+}
+
+export const logout = () => (dispatch) => {
+  dispatch({
+    type: LOGOUT,
+  })
+}
