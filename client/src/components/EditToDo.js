2d82d00b6d19ca0cab45328e5171f0e83259f77c
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-09-08 01:46:13 -0400

    Created separate form to edit existing to do items

---
+++ client/src/components/EditToDo.js
@@ -0,0 +1,34 @@
+// import React, { useState } from 'react'
+// // import { getItem, addItem } from '../../../redux/actions/items'
+// import { connect } from 'react-redux'
+// import PropTypes from 'prop-types'
+
+// function EditToDo({ getItem, addItem }) {
+//   const [formData, setFormData] = useState(getItem)
+
+//   const submit = (e) => {
+//     e.preventDefault()
+//     addItem(formData, true)
+//   }
+//   const updateForm = ({ target }) => {
+//     setFormData({ ...formData, [target.name]: target.value })
+//   }
+
+//   return (
+//     <form className='item' onSubmit={submit}>
+//       <div className='text'>
+//         <input name='text' value={formData.text} onInput={updateForm} />
+//       </div>
+//       <div className='btns'>
+//         <button type='submit'>Save</button>
+//       </div>
+//     </form>
+//   )
+// }
+
+// CreateToDo.propTypes = {
+//   getItem: PropTypes.func.isRequired,
+//   addItem: PropTypes.func.isRequired,
+// }
+
+// export default connect(null, { addItem, getItem })(CreateToDo)
